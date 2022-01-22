33332006a40286a0aea70c324365fe0bd9b7cd3e
Hurtownia <adam18jzic@gmail.com>
2022-01-22 15:32:50 +0100

    dodano metody zostało maina

--- snow.py
+++ snow.py
@@ -1,5 +1,5 @@
 from bitstring import BitArray, BitStream
-import tabele
+import snow_arrays
 import clock
 
 # deklaracja 32 bitowego  LFSR:
@@ -32,35 +32,60 @@ def MUL_alpha(w):
         a = w<<1
         if(a>>32 == 1):
             a^=s+1
-    result = (a^tabele.snow_alpha_mul[index])
+    result = (a ^ snow_arrays.snow_alpha_mul[index])
     return result
 
 
 def MUL_alpha_iverted(w):
     index = (w & 0xFF)
     a = w>>8
-    result = a^tabele.snow_alpha_mul_inv[index]
+    result = a ^ snow_arrays.snow_alpha_mul_inv[index]
     return result
 
+
+#def SR(x):
+    #return (tabele.Sbox[b] for b in x)
+
 def S(w):
-    r0,r1,r2,r3 = 0,0,0,0
-    #todo naprawa
-    srw0=SR[(w[:8]).uint]
-    srw1=SR[(w[8:16]).uint]
-    srw2=SR[(w[16:24]).uint]
-    srw3=SR[(w[24:]).uint]
+    w0 = w[:8]
+    w1 = w[8:16]
+    w2 = w[16:24]
+    w3 = w[24:]
 
-    srw0_new=BitArray(srw0)
-    srw1_new=BitArray(srw1)
-    srw2_new=BitArray(srw2)
-    srw3_new=BitArray(srw3)
+    r = snow_arrays.snow_T0[w0] ^ snow_arrays.snow_T1[w1] ^ snow_arrays.snow_T2[w2] ^ snow_arrays.snow_T3[w3]
+    return r
 
-    r0=(MULx(srw0_new,cte)^(srw1_new)^(srw2_new)^(MULx(srw3_new,cte)^(srw3_new)))
-    r1=((MULx(srw0_new,cte)^(srw0_new))^MULx(srw1_new,cte)^(srw2_new)^(srw3_new))
-    r2=((srw0_new)^(MULx(srw1_new,cte)^(srw1_new))^MULx(srw2_new,cte)^(srw3_new))
-    r3=((srw0_new)^(srw1_new)^(MULx(srw2_new,cte)^(srw2_new))^MULx(srw3_new,cte))
+def Initialize(k,IV):
+    global LFSR_S0, LFSR_S1, LFSR_S2, LFSR_S3, LFSR_S4, LFSR_S5, LFSR_S6, LFSR_S7, LFSR_S8, LFSR_S9, LFSR_S10, LFSR_S11, LFSR_S12, LFSR_S13, LFSR_S14, LFSR_S15, FSM_R1, FSM_R2
+    LFSR_S15 = k[3] ^ IV[0]
+    LFSR_S14 = k[2]
+    LFSR_S13 = k[1]
+    LFSR_S12 = k[0] ^ IV[1]
+    LFSR_S11 = k[3] ^ 0xffffffff #
+    LFSR_S10 = k[2] ^ 0xffffffff ^ IV[2]
+    LFSR_S9 = k[1] ^ BitArray('0xffffffff') ^ IV[3]
+    LFSR_S8 = k[0] ^ BitArray('0xffffffff')
+    LFSR_S7 = k[3]
+    LFSR_S6 = k[2]
+    LFSR_S5 = k[1]
+    LFSR_S4 = k[0]
+    LFSR_S3 = k[3] ^ BitArray('0xffffffff')
+    LFSR_S2 = k[2] ^ BitArray('0xffffffff')
+    LFSR_S1 = k[1] ^ 0xffffffff
+    LFSR_S0 = k[0] ^ 0xffffffff
+    FSM_R1 = BitArray('0x00000000')
+    FSM_R2 = BitArray('0x00000000')
+    for i in range(32):
+        F = clock.ClockFSM()
+        clock.Clock_init_LFSR(F)
 
-    return (r0+r1+r2+r3)
+def GenerateKeystream(n):
+    clock.ClockFSM()
+    clock.Clock_work_LFSR()
+    z = []
 
-def Initialize():
-# todo zrobić
+    for d in range(n):
+        F = clock.ClockFSM()
+        z.append(F^LFSR_S0)
+        clock.Clock_work_LFSR()
+	return z
