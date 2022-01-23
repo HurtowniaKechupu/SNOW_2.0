cd551491cf06da9e6db061b776675c547b00fbc3
Hurtownia <adam18jzic@gmail.com>
2022-01-23 16:34:23 +0100

    no działa

--- main.py
+++ main.py
@@ -11,8 +11,31 @@ def Trim_32(x):
     else:
         return x
 
+def MULx(v,c):
+    if v.bin[0]=='1':
+        return ((v<<1)^c)
+    else:
+        return (v<<1)
 
-def MUL_alpha(w):
+
+def MULxPOW(v,i,c):
+    if i==0:
+        return v
+    else:
+        return MULx(MULxPOW(v,i-1,c),c)
+
+
+def MUL_alpha(c):
+    #c = c[:8]
+    x=BitArray('0xa9')
+    return ((MULxPOW(c,23,x))+(MULxPOW(c,245,x))+(MULxPOW(c,48,x))+(MULxPOW(c,239,x)))
+
+def DIV_alpha(c):
+    #c = c[24:]
+    x=BitArray('0xa9')
+    return ((MULxPOW(c,16,x))+(MULxPOW(c,39,x))+(MULxPOW(c,6,x))+(MULxPOW(c,64,x)))
+
+def MUL_alpha2(w):
     index = w >> 24 # '>>'  	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
     index = index.uint
     a = 0
@@ -20,22 +43,21 @@ def MUL_alpha(w):
         a = w<<1
         if(a>>32 == 1):
             a^=s+1
-    b = BitArray(snow_arrays.snow_alpha_mul[index])
+    b = Trim_32(BitArray(snow_arrays.snow_alpha_mul[index]))
     result = (a ^ b)
     return result
 
 
-def MUL_alpha_iverted(w):
+def DIV_alpha2(w):
     index = (Trim_32(w) & '0x000000FF')
     index = index.uint
     a = w >> 8
-    b = BitArray(snow_arrays.snow_alpha_mul_inv[index])
+    b = Trim_32(BitArray(snow_arrays.snow_alpha_mul_inv[index]))
     result = a ^ b
     return result
 
 
-def S(w):
-    # todo warto tu też poszukać
+def S(w): #TA METODA JEST DOBRZE NA 99% NIE RUSZAĆ JEJ
     w = BitArray(w)
     w3 = w[:8].uint
     w2 = w[8:16].uint
@@ -43,6 +65,7 @@ def S(w):
     w0 = w[24:].uint
     r = Trim_32(BitArray(snow_arrays.snow_T0[w0])) ^ Trim_32(BitArray(snow_arrays.snow_T1[w1])) ^ Trim_32(BitArray(snow_arrays.snow_T2[w2])) ^ Trim_32(BitArray(snow_arrays.snow_T3[w3]))
     return r
+
 
 def Initialize(k,IV):
     global LFSR_S0, LFSR_S1, LFSR_S2, LFSR_S3, LFSR_S4, LFSR_S5, LFSR_S6, LFSR_S7, LFSR_S8, LFSR_S9, LFSR_S10, LFSR_S11, LFSR_S12, LFSR_S13, LFSR_S14, LFSR_S15, FSM_R1, FSM_R2
@@ -83,7 +106,8 @@ def GenerateKeystream(n):
 
 def Clock_init_LFSR(F):
     global LFSR_S0,LFSR_S1, LFSR_S2, LFSR_S3, LFSR_S4, LFSR_S5, LFSR_S6, LFSR_S7, LFSR_S8, LFSR_S9, LFSR_S10, LFSR_S11, LFSR_S12, LFSR_S13, LFSR_S14, LFSR_S15
-    v = F ^ MUL_alpha(LFSR_S0) ^ LFSR_S2 ^ MUL_alpha_iverted(LFSR_S11)
+    #v = F ^ MUL_alpha(LFSR_S0) ^ LFSR_S2 ^ DIV_alpha(LFSR_S11)
+    v = ((LFSR_S0 << 8) ^ MUL_alpha(BitArray(LFSR_S0[:8])) ^ (LFSR_S2) ^ (LFSR_S11 >> 8) ^ DIV_alpha(BitArray(LFSR_S11[24:])) ^ F)
     LFSR_S0=LFSR_S1
     LFSR_S1=LFSR_S2
     LFSR_S2=LFSR_S3
@@ -105,7 +129,8 @@ def Clock_init_LFSR(F):
 # zegar działaanie
 def Clock_work_LFSR():
     global LFSR_S0, LFSR_S1, LFSR_S2, LFSR_S3, LFSR_S4, LFSR_S5, LFSR_S6, LFSR_S7, LFSR_S8, LFSR_S9, LFSR_S10,LFSR_S11, LFSR_S12, LFSR_S13, LFSR_S14, LFSR_S15
-    v = MUL_alpha(LFSR_S0) ^ LFSR_S2 ^ MUL_alpha_iverted(LFSR_S11)
+    #v = MUL_alpha(LFSR_S0) ^ LFSR_S2 ^ DIV_alpha(LFSR_S11)
+    v = ((LFSR_S0 << 8) ^ MUL_alpha(BitArray(LFSR_S0[:8])) ^ (LFSR_S2) ^ (LFSR_S11 >> 8) ^ DIV_alpha(BitArray(LFSR_S11[24:])))
     LFSR_S0 = LFSR_S1
     LFSR_S1 = LFSR_S2
     LFSR_S2 = LFSR_S3
@@ -163,7 +188,7 @@ s = BitArray('0xFFFFFFFF') # 4,294,967,295
 
 #todo obecny problem to niezgodność wygenerowanego z testowym
 # drugi zestaw testowy*
-# 8D590AE9, A74A7D05, 6DC9CA74, B72D1A45, 99B0A083
+# E00982F5, 25F02054, 214992D8, 706F2B20, DA585E5B dla AAAAA,(0,0,0,0)
 key = [BitArray('0xAAAAAAAA'),BitArray('0xAAAAAAAA'),BitArray('0xAAAAAAAA'),BitArray('0xAAAAAAAA')]
 k = [key[3],key[2],key[1],key[0]] # nie wiem czy trzeba odwrócić kolejność czy nie, sprawdzić później
 IV = [BitArray('0x00000000'),BitArray('0x00000000'),BitArray('0x00000000'),BitArray('0x00000000')]
@@ -171,3 +196,4 @@ iv = [IV[3],IV[2],IV[1],IV[0]]# nie wiem czy trzeba odwrócić kolejność czy n
 Initialize(k,iv)
 keystream = GenerateKeystream(5)
 print(keystream)
+
